f6a04b908e7aea3d804a360cb6f351c783d3117e
amerjustice <amerjustice@outlook.com>
2016-05-07 15:31:46 -0700

    Initial Commit

---
+++ Pig Latin/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Pig Latin
+//
+//  Created by Susan Justice on 2016-05-07.
+//  Copyright © 2016 Amer Justice. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
